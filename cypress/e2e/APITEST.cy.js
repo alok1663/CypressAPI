08a79c23b2e73a46ebf6655c0db03c82853ede83
alok1663 <alok1663@gmail.com>
2024-06-22 17:31:34 +0530

    Initial commit: Added Cypress tests for API testing

---
+++ cypress/e2e/APITEST.cy.js
@@ -0,0 +1,111 @@
+describe('GetCall function', () => {
+  let accessToken = '6f28e621b66a4cb272f29b7ce245bb9565ee4f8aaa6299ece3fa8e8003ea838d';
+  let gurl = 'https://gorest.co.in/public/v2';
+  const userName = Math.random().toString(36).substring(2, 15);
+  const emailId = userName + "@apple.com";
+  let id1 = Math.floor(Math.random() * 10000);
+
+  it('GET scenario', () => {
+    cy.request({
+      url: gurl + '/users/',
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      }
+    }).then((response) => {
+      cy.log(JSON.stringify(response));
+      expect(response.status).to.eq(200);
+      //expect(response.body.id).to.eq(6975881);
+    });
+  });
+
+  it('POST', () => {
+    cy.request({
+      url: gurl + '/users',
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      body: {
+        "id": id1,
+        "name": userName,
+        "email": emailId,
+        "gender": "male",
+        "status": "active"
+      }
+    }).then((response) => {
+      cy.log(JSON.stringify(response));
+      expect(response.status).to.eq(201);
+      expect(response.body.email).to.eq(emailId);
+      expect(response.body.status).to.eq("active");
+    });
+  });
+
+  it('PUT check', () => {
+    cy.request({
+      method: "PUT",
+      url: gurl + '/users/6940121',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      body: {
+        status: "inactive"
+      }
+    }).then((response) => {
+      cy.log(JSON.stringify(response));
+      expect(response.body.status).to.eq("active");
+    });
+  });
+
+  it('DELETE', () => {
+    cy.request({
+      method: "DELETE",
+      url: gurl + '/users/6975881',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(204);
+    });
+  });
+
+  it('GET nested', () => {
+    cy.request({
+      method: 'GET',
+      url: gurl + '/users/6940121/posts',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      }
+    }).then((response) => {
+      cy.writeFile('cypress/fixtures/response.txt', JSON.stringify(response));
+      expect(response.status).to.eq(200);
+    });
+  });
+
+
+
+
+//DEMO QUERIES
+
+it('Demo' , () => { 
+
+Math.floor(Math.random()*10000);
+
+Math.random().toString(26).substring(15,2);
+
+username = Math.raondom().tostring(36).substring(15,2);
+
+
+
+})
+
+
+
+
+
+});
+
+
+
+
+
---
+++ cypress/e2e/socast.cy.js
@@ -0,0 +1,134 @@
+describe('template spec', () => {
+
+  beforeEach(() => {
+   cy.viewport(1920, 1080);
+    cy.visit('https://www.socastdigital.com/');
+    cy.wait(2000);
+  });
+
+  it('careerPage', () => {
+    cy.contains('a', 'Who We Are').focus();
+    cy.contains('a', 'Who We Are').invoke('show');
+    cy.contains('a', 'Who We Are').realHover();
+    cy.wait(2000);
+    cy.contains('Careers').click();
+  });
+
+  it('AboutPage', () => {
+   
+    cy.contains('Who We Are').realHover();
+    cy.wait(2000);
+    cy.contains('About').click();
+  });
+
+
+  it('EngagePage', () => {
+    cy.contains('a', 'Solutions').realHover();
+    cy.wait(2000);
+    cy.contains('a', 'Engage').click();
+    // cy.get('.vc_general.vc_btn3.vc_btn3-size-md.vc_btn3-shape-square.vc_btn3-style-modern.vc_btn3-icon-left.vc_btn3-color-white').click();
+  });
+
+  it('schedule a demo Page', () => {
+    cy.get('nav#stickyHeader li:nth-child(6) a:nth-child(1)').click();
+    cy.get('#First_Name').type('Alok');
+    cy.get('#Last_Name').type('Deep');
+    cy.get('#Email').type('Email@email.com');
+    cy.get('#Company').type('DemoCOMPANY');
+    cy.get('#LEADCF3').select('Executive/Market President/C-Suite');
+    cy.get('#Phone').type('433435435').click();
+  });
+
+  it('REACHPAGE', () => {
+    cy.contains('a', 'Solutions').realHover();
+    cy.contains('Reach').dblclick();
+  });
+
+  it('ResourcePage', () => {
+   
+    cy.contains('Resources').realHover();
+    cy.wait(2000);
+    cy.contains('Media').click();
+  });
+
+  it('Success Stories', () => {
+   
+    cy.contains('Resources').realHover();
+    cy.wait(2000);
+    cy.contains('Success Stories').click();
+  });
+
+  it('SoCast Support Centre', () => {
+   
+    cy.contains('Support').realHover();
+    cy.wait(2000);
+    cy.contains('SoCast Support Centre').dblclick();
+    cy.get('a[data-id="kb_module"]').click();
+    cy.wait(3000)
+    cy.contains('Getting Started').click();
+    cy.contains('Sample Websites and Mobile Apps').click();
+    cy.wait(3000);
+    cy.contains('Knowledge Base').click();
+
+  });
+
+  it('Digital Expertise Page', () => {
+   
+    cy.contains('Support').realHover();
+    cy.wait(2000);
+    cy.contains('Digital Expertise On Demand').click();
+  });
+
+  it('Contact Us_Page', () => {
+   
+    cy.contains('Support').realHover();
+    cy.wait(3000);
+    cy.contains('Contact Us').click();
+    cy.get('#First_Name').type('Alok');
+    cy.get('#Last_Name').type('Deep');
+    cy.get('#Email').type('Email@email.com');
+    cy.get('#Company').type('DemoCOMPANY');
+    cy.get('#LEADCF3').select('Executive/Market President/C-Suite');
+    cy.get('#Phone').type('433435435').click();
+  });
+
+  it('KnowledgeBAse',()=>{
+
+cy.visit('https://support.socastdigital.com/portal/en/home');
+
+cy.get('a[data-id="kb_module"]').click();
+cy.wait(3000);
+cy.get('a[data-id="KbCategory_495022000001141329"]').click();
+cy.get('a[data-id="article_495022000001142034"]').click();
+
+
+  })
+
+
+
+  it('TestSHouldhave' , () => {
+
+    cy.visit('https://www.socastdigital.com/careers/');
+
+    cy.get('h1').should('have.text', 'Careers at SoCast');
+    cy.get('strong').should('have.text', 'Elliott Hurst');
+    cy.wait(3000);
+    cy.get('a[href="https://socastdigital.zohorecruit.com/jobs/Careers/333867000003877009/Sales-Development-Representative?source=CareerSite"]').click();
+  })
+
+
+  it('TestSHouldhave' , () => {
+
+    cy.visit('https://www.socastdigital.com/careers/');
+
+    cy.get('h1').should('have.text', 'Careers at SoCast');
+    cy.get('strong').should('have.text', 'Elliott Hurst');
+    cy.wait(3000);
+    cy.get('a[href="https://socastdigital.zohorecruit.com/jobs/Careers/333867000003877009/Sales-Development-Representative?source=CareerSite"]').click();
+  })
+
+});
+
+
+
+
